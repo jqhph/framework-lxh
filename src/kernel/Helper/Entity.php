e788dfa815a10ccf2d4b6b8065320b8e813ecade
Jqh <841324345@qq.com>
2017-09-18 15:41:45 +0800

    实体增加实现ArrayAccess接口

--- src/kernel/Helper/Entity.php
+++ src/kernel/Helper/Entity.php
@@ -10,8 +10,9 @@ namespace Lxh\Helper;
 
 use Lxh\Contracts\Support\Arrayable;
 use Lxh\Contracts\Support\Jsonable;
+use ArrayAccess;
 
-class Entity implements Arrayable, Jsonable
+class Entity implements ArrayAccess, Arrayable, Jsonable
 {
     /**
      * 属性
@@ -29,29 +30,49 @@ class Entity implements Arrayable, Jsonable
 
     public function __construct(array $data = [])
     {
-        $this->items = $data;
+        $this->items = &$data;
     }
 
-    // 回收数据
+    /**
+     * 回收数据
+     *
+     * @param string $name
+     * @return void
+     */
     public function recycle($name)
     {
         $this->recycled[$name] = $this->items[$name];
         unset($this->items[$name]);
     }
 
-    // 删除数据
+    /**
+     * 删除数据
+     *
+     * @param string $name
+     * @return void
+     */
     public function remove($name)
     {
         unset($this->items[$name]);
     }
 
-    // 判断属性是否存在
+    /**
+     * 判断属性是否存在
+     *
+     * @param string $name
+     * @return bool
+     */
     public function has($name)
     {
         return isset($this->items[$name]);
     }
 
-    // 注入属性
+    /**
+     * 注入属性
+     *
+     * @param array $data
+     * @return void
+     */
     public function fill(array $data)
     {
         $this->items = array_merge($this->items, $data);
@@ -64,12 +85,8 @@ class Entity implements Arrayable, Jsonable
      * @param  $default mixed 默认值
      * @return mixed
      */
-    public function get($key = null, $default = null)
+    public function get($key, $default = null)
     {
-        if ($key === null) {
-            return $this->items;
-        }
-
         if (isset($this->items[$key])) return $this->items[$key];
 
         $lastItem = & $this->items;
@@ -84,55 +101,142 @@ class Entity implements Arrayable, Jsonable
         return $lastItem;
     }
 
-    // 设置属性值
+    /**
+     * 设置属性值
+     *
+     * @param string $name
+     * @param mixed $value
+     * @return static
+     */
     public function set($name, $value)
     {
         $this->items[$name] = $value;
         return $this;
     }
 
-    // 追加值到某一属性中
+    /**
+     * 追加值到某一属性中
+     *
+     * @param string $name
+     * @param mixed $value
+     * @return void
+     */
     public function append($name, $value)
     {
-        $this->items[$name][] = $value;
-        return $this;
+        $this->items[$name][] = &$value;
     }
 
-    // 清除属性
+    /**
+     * 清除属性
+     *
+     * @return void
+     */
     public function reset()
     {
         $this->items = [];
     }
 
+    /**
+     * 获取所有属性
+     *
+     * @return array
+     */
     public function all()
     {
         return $this->items;
     }
 
+    /**
+     * 以数组形式返回所有属性
+     *
+     * @return array
+     */
     public function toArray()
     {
         return $this->items;
     }
 
+    /**
+     * 返回json格式所有属性数据
+     *
+     * @param mixed $options
+     * @return string
+     */
     public function toJson($options = 0)
     {
         return json_encode($this->items);
     }
 
-    // 获取属性值
+    /**
+     * 获取属性值
+     *
+     * @param string $name
+     * @return mixed
+     */
     public function __get($name)
     {
         return isset($this->items[$name]) ? $this->items[$name] : null;
     }
 
-    // 设置属性值
+    /**
+     * 设置属性值
+     *
+     * @param string $name
+     * @param mixed $value
+     * @return void
+     */
     public function __set($name, $value)
     {
-        $this->items[$name] = $value;
+        $this->items[$name] = &$value;
     }
 
     public function __toString()
     {
         return json_encode($this->items);
+    }
+
+    /**
+     * Determine if a given offset exists.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function offsetExists($key)
+    {
+        return isset($this->instances[$key]);
+    }
+
+    /**
+     * Get the value at a given offset.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function offsetGet($key)
+    {
+        return $this->get($key);
+    }
+
+    /**
+     * Set the value at a given offset.
+     *
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return void
+     */
+    public function offsetSet($key, $value)
+    {
+        $this->items[$key] = &$value;
+    }
+
+    /**
+     * Unset the value at a given offset.
+     *
+     * @param  string  $key
+     * @return void
+     */
+    public function offsetUnset($key)
+    {
+        unset($this->items[$key]);
     }
 }
