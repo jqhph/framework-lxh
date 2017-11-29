df94de2d38dcd1b56de1726095762c4ee33c3f0c
Jqh <841324345@qq.com>
2017-11-29 18:07:06 +0800

    卸载无效文件

--- src/application/Admin/Controller/Test.php
+++ src/application/Admin/Controller/Test.php
@@ -17,6 +17,7 @@ class Test extends Controller
 {
     public function actionTest(Request $req, Response $resp, & $params)
     {
+        
 //        query()->from('test1')->find();
         return $this->render('test', ['title' => 'HELLO']);
 //        $test = new \Lxh\Mails\Test();
