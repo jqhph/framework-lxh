1e3ad1de9ef6b006d3a29c697efab89e56a4594b
Jqh <841324345@qq.com>
2018-01-03 21:44:05 +0800

    pjax加载完毕重新绑定Grid行选择器事件

--- public/assets/primary/Admin/view/public-index.js
+++ public/assets/primary/Admin/view/public-index.js
@@ -88,68 +88,78 @@ define(['css/sweet-alert.min.css', 'lib/js/sweet-alert.min'], function () {
     // 绑定删除事件
     $('a[data-action="delete-row"]').click(public.delete);
     $('#batch-delete').click(public.batchDelete)
-    $(document).on('pjax:complete', function () {
-        // 绑定删除事件
-        $('a[data-action="delete-row"]').click(public.delete);
-    });
     add_action(function () {
         // 行选择器点击事件
-        (function () {
-            var allInput = $('input[data-action="select-all"]')
-            // 选中所有行checkbox点击事件
-            allInput.click(function () {
-                var _this = $(this), tb = _this.parent().parent().parent().parent(), inputs = tb.find('input[name="tb-row[]"]');
-                if (_this.prop('checked')) {
-                    // 选中所有行，并把所有行的id存储到本按钮value中
-                    inputs.prop('checked', true);
-                    var ids = [], i, id;
-                    for (i in inputs) {
-                        if (typeof inputs[i] != 'object' || typeof inputs[i] == 'function' || typeof $(inputs[i]).val == 'undefined') continue;
-                        id = $(inputs[i]).val();
-                        if (!id) continue;
-                        ids.push(id);
-                        active($(inputs[i])); // 添加选中效果
-                    }
-                    set_all_input(ids.join(','));
+        var allInput = $('input[data-action="select-all"]');
 
-                } else {
-                    inputs.prop('checked', false);
-                    set_all_input(''); // 清除值
-                    for (i in inputs) {
-                        if (typeof inputs[i] != 'object' || typeof inputs[i] == 'function' || typeof $(inputs[i]).val == 'undefined') continue;
-                        active($(inputs[i]), false) // 移除选中效果
-                    }
-                }
-            });
-            function set_all_input(val) {
-                listids = val;
-                allInput.val(val);
-                $(document).trigger('grid.selected', val);
-            }
+        $(document).on('pjax:complete', function () {
+            // 绑定删除事件
+            $('a[data-action="delete-row"]').click(public.delete);
+
+            allInput = $('input[data-action="select-all"]');
+            // 反选点击事件
+            allInput.click(selectall);
             // 单行选中事件
-            $('input[name="tb-row[]"]').click(function () {
-                var ids = allInput.val(), $this = $(this), id = $this.val();
-                ids = (ids !== 'on' && ids) ? ids.split(',') : [];
-                if ($this.prop('checked')) {
-                    if (id) ids.push(id);
-                    active($this);
-                } else {
-                    for(var i in ids) {
-                        if(ids[i] == id) {
-                            ids.splice(i, 1);
-                            break;
-                        }
-                    }
-                    active($this, false);
+            $('input[name="tb-row[]"]').click(selecone);
+        });
+
+        // 反选点击事件
+        allInput.click(selectall);
+        // 单行选中事件
+        $('input[name="tb-row[]"]').click(selecone);
+
+        function selectall() {
+            var _this = $(this), tb = _this.parent().parent().parent().parent(), inputs = tb.find('input[name="tb-row[]"]');
+            if (_this.prop('checked')) {
+                // 选中所有行，并把所有行的id存储到本按钮value中
+                inputs.prop('checked', true);
+                var ids = [], i, id;
+                for (i in inputs) {
+                    if (typeof inputs[i] != 'object' || typeof inputs[i] == 'function' || typeof $(inputs[i]).val == 'undefined') continue;
+                    id = $(inputs[i]).val();
+                    if (!id) continue;
+                    ids.push(id);
+                    active($(inputs[i])); // 添加选中效果
                 }
-                set_all_input(ids.join(','))
-            })
-            // 给当前行添加选中效果
-            function active(input, close) {
-                if (input.data('action') == 'select-all') return;
-                var tr = input.parent().parent();
-                tr.removeClass('active');
-                if (close !== false) tr.addClass('active');
+                set_all_input(ids.join(','));
+
+            } else {
+                inputs.prop('checked', false);
+                set_all_input(''); // 清除值
+                for (i in inputs) {
+                    if (typeof inputs[i] != 'object' || typeof inputs[i] == 'function' || typeof $(inputs[i]).val == 'undefined') continue;
+                    active($(inputs[i]), false) // 移除选中效果
+                }
             }
-        })()    })
+        }
+        function set_all_input(val) {
+            listids = val;
+            allInput.val(val);
+            $(document).trigger('grid.selected', val);
+        }
+        function selecone() {
+            var ids = allInput.val(), $this = $(this), id = $this.val();
+            ids = (ids !== 'on' && ids) ? ids.split(',') : [];
+            if ($this.prop('checked')) {
+                if (id) ids.push(id);
+                active($this);
+            } else {
+                for(var i in ids) {
+                    if(ids[i] == id) {
+                        ids.splice(i, 1);
+                        break;
+                    }
+                }
+                active($this, false);
+            }
+            set_all_input(ids.join(','))
+        }
+        // 给当前行添加选中效果
+        function active(input, close) {
+            if (input.data('action') == 'select-all') return;
+            var tr = input.parent().parent();
+            tr.removeClass('active');
+            if (close !== false) tr.addClass('active');
+        }
+    })
 });
