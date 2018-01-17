24dddcb2ea02ab977b013ebf3af630821a095caa
Jqh <841324345@qq.com>
2018-01-17 10:12:59 +0800

    路由配置文件简化

--- src/config/route/route.php
+++ src/config/route/route.php
@@ -6,38 +6,36 @@
  * Time: 10:01
  */
 
-$config = [];
+return [
+    [
+        'pattern' => '/',
+        'method' => 'GET',
+        'params' => [
+            'controller' => 'Index',
+            'action' => 'List'
+        ]
+    ],
 
-$config[] = [
-    'pattern' => '/',
-    'method' => 'GET',
-    'params' => [
-        'controller' => 'Index',
-        'action' => 'List'
+    [
+        'pattern' => '/dev',
+        'method' => 'GET',
+        'params' => [
+            'auth' => false,
+            'module' => 'Admin',
+            'controller' => 'Test',
+            'action' => 'Test'
+        ]
+    ],
+
+    [
+        'pattern' => '*',
+        'method' => '*',
+        'params' => [
+            'auth' => false,
+            'module' => 'Admin',
+            'controller' => 'App',
+            'action' => 'NotFound'
+        ]
     ]
 ];
 
-$config[] = [
-    'pattern' => '/dev',
-    'method' => 'GET',
-    'params' => [
-        'auth' => false,
-        'module' => 'Admin',
-        'controller' => 'Test',
-        'action' => 'Test'
-    ]
-];
-
-// 匹配任意路由
-$config[] = [
-    'pattern' => '*',
-    'method' => '*',
-    'params' => [
-        'auth' => false,
-        'module' => 'Admin',
-        'controller' => 'App',
-        'action' => 'NotFound'
-    ]
-];
-
-return $config;
