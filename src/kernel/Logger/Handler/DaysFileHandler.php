b99a0f8ae9955190a36b863eeda2be5c4352c626
Jqh <841324345@qq.com>
2018-01-22 11:30:04 +0800

    日志路径配置支持绝对路径

--- src/kernel/Logger/Handler/DaysFileHandler.php
+++ src/kernel/Logger/Handler/DaysFileHandler.php
@@ -9,32 +9,66 @@ use Lxh\Contracts\Container\Container;
  */
 class DaysFileHandler extends \Monolog\Handler\StreamHandler 
 {
+	/**
+	 * @var string
+	 */
 	protected $dateFormat = 'Y-m-d';
-	
+
+	/**
+	 * @var string
+	 */
 	protected $filenameFormat = '{filename}-{date}';
-	
-	//项目根目录
-	private $rootPre = __ROOT__;
-	
+
+	/**
+	 * 项目根目录
+	 *
+	 * @var string
+	 */
+	private $root = __ROOT__;
+
+	/**
+	 * @var string
+	 */
 	private $separator = DIRECTORY_SEPARATOR;
-	
+
+	/**
+	 * @var int
+	 */
 	private $maxFiles = 180;//目录下日志文件最大数量。0为不限制
-	
-	private $fileManager;
+
+	/**
+	 * @var \Lxh\File\FileManager
+	 */
+	private $files;
 	
 	public function __construct($stream, $level = Logger::DEBUG, $bubble = true, $filePermission = null, $useLocking = false) 
 	{
-		$this->fileManager = files();
-		
-		$this->filename = $this->rootPre . $stream;
+		$this->files = files();
+		$this->filename = $this->normalizePath($stream);
 		
 		$this->removeExcessFile();//删除超出的文件
-		
 		$this->formatFilename($this->filename);
 		
 		parent::__construct($this->filename, $level, $bubble, $filePermission);
 	}
-	
+
+	/**
+	 * @param $path
+	 * @return string
+	 */
+	protected function normalizePath(&$path)
+	{
+		if (strpos($path, '/') === 0 || strpos($path, ':')) {
+			return $path;
+		}
+		return $this->root . $path;
+	}
+
+	/**
+	 *
+	 * @param $stream
+	 * @return bool
+	 */
 	private function formatFilename(& $stream) 
 	{
 		if (! is_string($stream)) {
@@ -70,8 +104,8 @@ class DaysFileHandler extends \Monolog\Handler\StreamHandler
         }
 
         $filePattern = $this->getFilePattern();
-        $dirPath = $this->fileManager->getDirName($this->filename);
-        $logFiles = $this->fileManager->getFileList($dirPath, false, $filePattern, true);
+        $dirPath = $this->files->getDirName($this->filename);
+        $logFiles = $this->files->getFileList($dirPath, false, $filePattern, true);
 
         if (! empty( $logFiles) && count($logFiles) > $this->maxFiles) {
 
@@ -81,10 +115,13 @@ class DaysFileHandler extends \Monolog\Handler\StreamHandler
 
             $logFilesToBeRemoved = array_slice($logFiles, $this->maxFiles);
 
-            $this->fileManager->removeFile($logFilesToBeRemoved, $dirPath);
+            $this->files->removeFile($logFilesToBeRemoved, $dirPath);
         }
     }
 
+	/**
+	 * @return mixed|string
+	 */
     protected function getFilePattern() 
     {
         $fileInfo = pathinfo($this->filename);
