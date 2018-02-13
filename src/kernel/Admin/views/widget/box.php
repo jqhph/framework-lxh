541cca42565df450e522d6cd40d4278cc4733d28
Jqh <841324345@qq.com>
2018-02-13 18:32:38 +0800

    样式优化

--- src/kernel/Admin/views/widget/box.php
+++ src/kernel/Admin/views/widget/box.php
@@ -3,9 +3,7 @@
         <h3 class="portlet-title"><?php echo $title ?></h3>
         <span class="dropdown"><?php echo $actions?></span>
         <div class="portlet-widgets">
-            <div class="<?php echo $toolClass?>"><div class="btn-group dropdown-btn-group pull-right">
-            <?php echo $tools; ?>
-            </div></div>
+            <div class="<?php echo $toolClass?>"><div class="dropdown-btn-group pull-right" style="margin-left:5px;"><?php echo $tools; ?></div></div>
         </div>
         <div style="clear:both"></div>
     </div>
