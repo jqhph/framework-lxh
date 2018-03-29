5069be32d5f99f9bef230f3138231337e0c82549
Jqh <841324345@qq.com>
2018-03-29 16:10:54 +0800

    分页样式美化

--- src/kernel/Admin/views/grid-content.php
+++ src/kernel/Admin/views/grid-content.php
@@ -4,7 +4,7 @@
         <div class="dataTables_paginate paging_simple_numbers pull-left">
             <ul class="pagination" style="float:right"><?php echo $pageString;?></ul>
             <?php if ($pageOptions) {?>
-                <select class="input-sm grid-per-pager" name="per-page"  style="float:right;margin-top:10px;margin-right:10px;background:#fff;"><?php
+                <select class="form-shadow input-sm grid-per-pager" name="per-page"  style="float:right;margin-top:10px;margin-right:10px;background:#fff;"><?php
                     foreach ($pageOptions as &$row) :
                         $string = $url->query($perPageKey, $row)->string();
                         ?><option <?php if ($perPage == $row) echo 'selected';?> value="<?php echo $string?>"><?php echo $row;?></option><?php endforeach;?></select>
