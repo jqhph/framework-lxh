cd8363f46c2a4fcf22cfe25bfbc2defafd9cf7e1
Jqh <841324345@qq.com>
2018-04-10 17:28:36 +0800

    Form::editor 按钮显示bug修复

--- src/kernel/Admin/Form/Field/Editor.php
+++ src/kernel/Admin/Form/Field/Editor.php
@@ -29,21 +29,21 @@ class Editor extends Field
      * @var array
      */
     protected $options = [
-        'height' => 600,
-        'codeFold' => true,
+        'height'             => 600,
+        'codeFold'           => true,
         'saveHTMLToTextarea' => true, // 保存 HTML 到 Textarea
-        'searchReplace' => true,
+        'searchReplace'      => true,
 //        'htmlDecode' => 'style,script,iframe|on*', // 开启 HTML 标签解析，为了安全性，默认不开启
-        'emoji' => true,
-        'taskList' => true,
-        'tocm' => true,         // Using [TOCM]
-        'tex' => true,                   // 开启科学公式TeX语言支持，默认关闭
-        'flowChart' => 'true',             // 开启流程图支持，默认关闭
-        'sequenceDiagram' => true,       // 开启时序/序列图支持，默认关闭,
-        'imageUpload' => true,
-        'imageFormats' => ['jpg', 'jpeg', 'gif', 'png', 'bmp', 'webp'],
-        'imageUploadURL' => '',
-        'autoFocus' => false,
+        'emoji'              => true,
+        'taskList'           => true,
+        'tocm'               => true,         // Using [TOCM]
+        'tex'                => true,                   // 开启科学公式TeX语言支持，默认关闭
+        'flowChart'          => true,             // 开启流程图支持，默认关闭
+        'sequenceDiagram'    => true,       // 开启时序/序列图支持，默认关闭,
+        'imageUpload'        => true,
+        'imageFormats'       => ['jpg', 'jpeg', 'gif', 'png', 'bmp', 'webp'],
+        'imageUploadURL'     => '',
+        'autoFocus'          => false,
     ];
 
     /**
