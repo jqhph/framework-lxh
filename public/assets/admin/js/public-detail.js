64782b979242b14b9585bb046809caa5c9ec4683
Jqh <841324345@qq.com>
2018-04-16 18:09:10 +0800

    表单页面js bug修复

--- public/assets/admin/js/public-detail.js
+++ public/assets/admin/js/public-detail.js
@@ -1,32 +1,42 @@
-new LxhLoader(['@lxh/js/validate.min'], function () {
-    // 所有js加载完毕时间
-    $(document).on('app.completed', detail);
-    detail();
-    function detail() {
-        var v = $lxh.validator(window.formRules || [], submit);
-        var model = $lxh.createModel(),
-            notify = $lxh.ui().notify(),
-            n = NProgress;
-        function submit(e) {
-            notify.remove();
+(function () {
+    var completed = 0;
+    $(document).on('app.completed', function () {
+        completed = 1;
+    });
 
-            // 设置请求开始回调函数
-            model.on('start', function (api, method, data) {
-                n.start();
-            });
-
-            // 设置成功回调函数
-            model.on('success', function (data) {
-                // success
-                n.done();
-                notify.success(trans('success'));
-            });
-            model.on('any', function () {
-                n.done();
-            });
-
-            // 发起修改或新增操作
-            model.save()
+    new LxhLoader(['@lxh/js/validate.min'], function () {
+        // 所有js加载完毕时间
+        if (completed) {
+            detail();
+        } else {
+            $(document).on('app.completed', detail);
         }
-    }
-}).request();
+        function detail() {
+            var v = $lxh.validator(window.formRules || [], submit);
+            var model = $lxh.createModel(),
+                notify = $lxh.ui().notify(),
+                n = NProgress;
+            function submit(e) {
+                notify.remove();
+
+                // 设置请求开始回调函数
+                model.on('start', function (api, method, data) {
+                    n.start();
+                });
+
+                // 设置成功回调函数
+                model.on('success', function (data) {
+                    // success
+                    n.done();
+                    notify.success(trans('success'));
+                });
+                model.on('any', function () {
+                    n.done();
+                });
+
+                // 发起修改或新增操作
+                model.save()
+            }
+        }
+    }).request();
+})();
