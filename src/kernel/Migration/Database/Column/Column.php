a6c75845ed92e1ca06fd27f72950be695435b948
Jqh <841324345@qq.com>
2018-04-19 20:18:09 +0800

    增加Migration模块

--- src/kernel/Migration/Database/Column/Column.php
+++ src/kernel/Migration/Database/Column/Column.php
@@ -23,6 +23,11 @@ abstract class Column
      */
     protected $options = [];
 
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
     public function getName()
     {
         return $this->name;
